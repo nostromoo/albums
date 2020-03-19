c10429ab417dfac08ce06f66f2aaab57987ac8a7
Romain Pedepoy <romain@smartnsoft.com>
2020-03-19 14:08:11 +0100

    initial commit

---
+++ app/src/main/java/com/romain/pedepoy/albums/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.romain.pedepoy.albums
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
